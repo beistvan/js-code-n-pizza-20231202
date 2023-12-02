e729511ae9f58c15c50af0c4ccf66b8e7f71c46c
Beregszászi István <beistvan@gmail.com>
2023-12-02 09:51:56 +0100

    Reassign Variables In JavaScript

---
+++ reassign-variables.js
@@ -0,0 +1,17 @@
+let a = "Hello";
+let firstNumber = 5;
+let secondNumber = 20;
+
+let pizza;
+
+console.log(a);
+console.log(firstNumber);
+console.log(secondNumber);
+
+pizza = "Bolognai és ananászos";
+
+console.log(pizza);
+
+pizza = "Csípős magyaros";
+
+console.log(pizza);
