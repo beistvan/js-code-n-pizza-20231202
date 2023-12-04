b7ed6edd65cd607258ae536d87781bd65cf19f52
Beregszászi István <beistvan@gmail.com>
2023-12-04 14:58:09 +0100

    Strings And Numbers Concatenations

---
+++ strings-and-numbers-concatenations.js
@@ -0,0 +1,13 @@
+let mondoka1 = "Fekete bikapata kopog ";
+let mondoka2 = "a patika pepita kövén.";
+
+console.log(mondoka1);
+console.log(mondoka2);
+
+console.log(mondoka1 + mondoka2);
+
+console.log(mondoka1 + 40);
+console.log(30 + mondoka1);
+console.log(10 + 25 + mondoka1);
+console.log(mondoka1 + 10 + 25);
+console.log(mondoka1 + (10 + 25));
