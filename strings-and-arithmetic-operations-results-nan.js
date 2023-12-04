09340638b7108a3cf13baaaeac5882904455ba64
Beregszászi István <beistvan@gmail.com>
2023-12-04 15:00:16 +0100

    Strings And Rithmetic Operations Results NaN

---
+++ strings-and-arithmetic-operations-results-nan.js
@@ -0,0 +1,10 @@
+let mondoka1 = "Fekete bikapata kopog ";
+let mondoka2 = "a patika pepita kövén.";
+
+console.log(mondoka1);
+console.log(mondoka2);
+
+console.log(mondoka1 - mondoka2);
+console.log(mondoka1 * mondoka2);
+console.log(mondoka1 / mondoka2);
+console.log(mondoka1 % mondoka2);
