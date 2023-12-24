5a2c4399658fe12b58103c84b45d9eb29e8f89b5
Beregszászi István <beistvan@gmail.com>
2023-12-24 18:13:59 +0100

    Adding Numbers Shorthand In JavaScript

---
+++ adding-numbers-shorthand.js
@@ -0,0 +1,34 @@
+let number = 5;
+let anotherNumber = 12;
+
+number = number + anotherNumber;
+
+console.log(number); // 17
+
+number += anotherNumber;
+
+console.log(number); // 29
+
+number = number + 1;
+
+console.log(number); // 30
+
+number += 1;
+
+console.log(number); // 31
+
+number++;
+
+console.log(number); // 32
+
+number++;
+
+console.log(number); // 33
+
+number++;
+
+console.log(number); // 34
+
+++number;
+
+console.log(number); // 35
